52d96eaa3c77bde44257e0d060fb81a79c541d36
MissNAH <nazhoos23@gmail.com>
2023-08-12 02:20:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - printing without printf and puts
  *
- * Return: Always 1 (success)
+ * Return: Always (1)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\n" - Dora Korpar, 2015 - 10 - 19\n",
-			59);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", );
 	return (1);
-}
